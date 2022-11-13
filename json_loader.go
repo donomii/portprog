1d6029c6abde81b505d38e0838f503429c2a86d7
Jeremy Price <donomii@gmail.com>
2022-11-13 18:21:43 +0100

    Add zip dist

--- json_loader.go
+++ json_loader.go
@@ -8,10 +8,15 @@ import (
 )
 
 type Package struct {
-	Name, Zip, Url, Fetch, Plan string
-	Branch, Command             string // If this is git repository, what branch/tag do we check out before building?
-	BinDir, LibDir				string	//Where do we find the binary files, relative to the top level directory of the installed package?
-	BinDirs, Deletes []string
+	Name            string   //Name of the package
+	Zip             string   //Full archive name, including extension
+	Url             string   //URL to download the archive from
+	Fetch           string   //Fetch method (e.g. wget, curl, git, svn, etc)
+	Plan            string   //Build plan (e.g. make, cmake, etc)
+	Branch, Command string   // If this is git repository, what branch/tag do we check out before building?
+	BinDir, LibDir  string   //Where do we find the binary files after the build, relative to the top level directory of the installed package?
+	BinDirs         []string //If there are multiple directories to add to the path.  Both BinDir and BinDirs are added to the path.
+	Deletes         []string
 }
 
 func LoadJSON(filename string) Package {
